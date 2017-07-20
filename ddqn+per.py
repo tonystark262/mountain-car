c9f9b1d96178b5ac9650150920972bfb78542e9b
sankalp <sankalp2621998@gmail.com>
2017-07-20 19:21:10 +0530

    added per

--- ddqn+per.py
+++ ddqn+per.py
@@ -1,14 +1,57 @@
 import os
 import random
-import queue as Q
 
 import gym
 import numpy as np
+import queue as Q
 import tensorflow as tf
+import heapq as hq
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 env = gym.make("MountainCar-v0")
 env.reset()
+
+
+class tup(object):
+    def __init__(self,p,ob):
+        self.priority=p
+        self.item=ob
+
+    def __lt__(self, other):
+        if self.priority<other.priority:
+            return True
+        return False
+
+
+class memory(object):
+    def __init__(self):
+        self.size = 0
+        self.memory = []
+        hq.heapify(self.memory)
+
+    def append(self, p, ob):
+        if self.size == 10000:
+            self.pop()
+        else:
+            self.size += 1
+        hq.heappush(self.memory,tup(p,ob))
+
+    def pop(self):
+        if self.size == 0:
+            return False
+        return hq.heappop(self.memory)
+
+    def sample(self, n):
+        if n > self.size:
+            print("size exceeded")
+            exit()
+        else:
+            sample = []
+            for i in range(n):
+                x = self.pop()
+                sample.append(x.item)
+            self.size -= n
+            return sample
 
 
 class dqn(object):
@@ -31,7 +74,7 @@ class dqn(object):
         self.target_biases = {}
         self.create_nn()
         self.create_training_network()
-        self.memory = Q.PriorityQueue()
+        self.memory = memory()
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.global_variables_initializer())
 
@@ -67,21 +110,19 @@ class dqn(object):
         self.q_value_target = self.feed_forward_target(self.x)
         self.output = tf.reduce_sum(tf.multiply(self.q_value, self.a), reduction_indices=1)
         self.action = tf.argmax(self.q_value, 1)
-        self.error=tf.abs(self.output - self.y)
+        self.error = tf.abs(self.output - self.y)
         self.loss = tf.reduce_mean(tf.square(self.output - self.y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
     def append_to_memory(self, state, action, reward, next_state, done):
         one_hot_action = np.zeros(self.output_size)
         one_hot_action[action] = 1
-        self.memory.append((state, one_hot_action, reward, next_state, done))
-        if len(self.memory) > 10000:
-            self.memory.popleft()
-        if len(self.memory) > self.batch_size:
+        self.memory.append(-1000000000, (state, one_hot_action, reward, next_state, done))
+        if self.memory.size > self.batch_size:
             self.train()
 
     def train(self):
-        sample = random.sample(self.memory, self.batch_size)
+        sample = self.memory.sample(self.batch_size)
         train_x = [i[0] for i in sample]
         action = [i[1] for i in sample]
         reward = [i[2] for i in sample]
@@ -98,6 +139,9 @@ class dqn(object):
         action = np.array(action)
         self.dropout = 1.0
         self.sess.run(self.optimizer, feed_dict={self.x: train_x, self.y: train_y, self.a: action})
+        error = self.sess.run(self.error, feed_dict={self.x: train_x, self.y: train_y, self.a: action})
+        for i in range(self.batch_size):
+            self.memory.append(-error[i], sample[i])
 
     def copy_variables(self):
         for i in range(1, len(self.weights) + 1, 1):
